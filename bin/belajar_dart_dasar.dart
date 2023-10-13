7bf1ead6e6caf43299e581a1455947e8df6a2b7e
Muh Firdaus <muhammadfirdaus66538@gmail.com>
2023-10-13 16:14:29 +0700

    first commit

---
+++ bin/belajar_dart_dasar.dart
@@ -0,0 +1,5 @@
+import 'package:belajar_dart_dasar/belajar_dart_dasar.dart' as belajar_dart_dasar;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_dart_dasar.calculate()}!');
+}
---
+++ test/belajar_dart_dasar_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_dart_dasar/belajar_dart_dasar.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
