b4d7a8dd817ae64127d71163d4826f07eaa6c3f1
Muh Firdaus <muhammadfirdaus66538@gmail.com>
2023-10-15 20:30:41 +0700

    dart comment

---
+++ bin/comment.dart
@@ -0,0 +1,14 @@
+/// this is main function
+/// will excecuted by dart
+void main() {
+  // this is name variable
+  var name = "Muhammad Firdaus";
+
+  /**
+   *  this is multilane comment
+   * you can add more here
+   */
+  /*
+  */
+  print(name);
+}
