1277d4913308971a632761403c4b8c5451a4ec0a
Muh Firdaus <muhammadfirdaus66538@gmail.com>
2023-10-15 20:39:26 +0700

    dart number

---
+++ bin/number.dart
@@ -0,0 +1,18 @@
+void main() {
+  // tipe data number di dart ada 2 int dan double
+  // int untuk bilangan bulat dan double untuk bilangan desimal
+
+  int number1 = 10;
+  double number2 = 10.5;
+
+  print(number1);
+  print(number2);
+
+  // jika ingin membuat tipe data number yang bisa int ataupun double bisa menggunakan num
+
+  num number = 10;
+  print(number);
+
+  number = 10.5;
+  print(number);
+}
