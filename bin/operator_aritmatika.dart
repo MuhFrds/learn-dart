0f1b6b1f29675f97bf80542ae6597b091502f371
Muh Firdaus <muhammadfirdaus66538@gmail.com>
2023-10-16 16:13:48 +0700

    dart operator aritmatika

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,11 @@
+void main() {
+  var first = 10;
+  var second = 3;
+
+  print(first + second);
+  print(first - second);
+  print(first * second);
+  print(first / second);
+  print(first ~/ second);
+  print(first % second);
+}
