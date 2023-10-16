9d75989bafb98dce3833f2c8e3c9bdcb866773e9
Muh Firdaus <muhammadfirdaus66538@gmail.com>
2023-10-16 15:55:55 +0700

    dart converison boolean

---
+++ bin/converison_boolean.dart
@@ -0,0 +1,10 @@
+void main() {
+  var inputString = 'true';
+  var inputBoolean = inputString == 'true';
+  // konversi string ke boolean tidak bisa
+
+  var booleanToString = inputBoolean.toString();
+
+  print(inputBoolean);
+  print(booleanToString);
+}
