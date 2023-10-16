8a5a1b709eaf80f5624a0fc054b3b06e568ba7d3
Muh Firdaus <muhammadfirdaus66538@gmail.com>
2023-10-16 15:50:07 +0700

    dart converison

---
+++ bin/conversion.dart
@@ -0,0 +1,27 @@
+void main() {
+  // cara konversi di dart
+
+  var inputString = '1000';
+  // konversi string ke interger
+  var inputInt = int.parse(inputString);
+  // konversi string ke double
+  var inputDouble = double.parse(inputString);
+
+  print(inputString);
+  print(inputInt);
+  print(inputDouble);
+
+  // konversi dari number ke number
+
+  // integer ke double
+  // var intToDouble = inputInt.toDouble();
+
+  // double ke integer
+  // var doubleToInt = inputDouble.toInt();
+
+  // integer ke string
+  // var intToString = inputInt.toString();
+
+  // double ke string
+  // var doubleToString = inputDouble.toString();
+}
