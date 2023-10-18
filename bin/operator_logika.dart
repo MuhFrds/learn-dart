aac20abdd4709074950f2fe713681b8878725db7
Muh Firdaus <muhammadfirdaus66538@gmail.com>
2023-10-19 03:11:11 +0700

    dart operator logika

---
+++ bin/operator_logika.dart
@@ -0,0 +1,18 @@
+void main() {
+  var nilaiAkhir = 80;
+  var nilaiAbsen = 80;
+
+  var apakahNilaiAkhirBagus = nilaiAkhir >= 75;
+  var apakahNilaiAbsenBagus = nilaiAkhir >= 75;
+
+  print(apakahNilaiAkhirBagus);
+  print(apakahNilaiAbsenBagus);
+
+  // var lulus = apakahNilaiAkhirBagus && apakahNilaiAbsenBagus;
+  var lulus = apakahNilaiAkhirBagus || apakahNilaiAbsenBagus;
+
+  print(lulus);
+
+  print(!true);
+  print(!false);
+}
