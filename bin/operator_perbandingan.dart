1e3833b0adaa62217f52bced9bf8975360c758d5
Muh Firdaus <muhammadfirdaus66538@gmail.com>
2023-10-18 13:42:49 +0700

    dart operator perbandingan

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,11 @@
+void main() {
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+
+  print('eko' == 'eko');
+  print('eko' != 'eko');
+}
