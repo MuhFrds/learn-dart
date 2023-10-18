c21087f4a10c349212f6cf8ec6ceed478f472f60
Muh Firdaus <muhammadfirdaus66538@gmail.com>
2023-10-18 14:36:57 +0700

    dart operator increment & decrement

---
+++ bin/operator_penugasan.dart
@@ -0,0 +1,26 @@
+void main() {
+  var a = 10;
+  num b = 10;
+
+  // a = a + 10;
+  a += 10;
+  a -= 10;
+  a *= 10;
+  b /= 10;
+  b ~/= 10;
+  a % 10;
+
+  print(a);
+  print(b);
+
+  // increment dan decrement
+
+  var i = 0;
+  // i++; // i = i + 1;
+
+  var j = i++; // j = 1, i++
+  var k = ++i; // k = (++i)
+
+  print(i);
+  print(j);
+}
