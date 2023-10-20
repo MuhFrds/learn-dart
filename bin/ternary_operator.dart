81d9392dc5f28b4e605fa9a44a8eb0dc659fc234
Muh Firdaus <muhammadfirdaus66538@gmail.com>
2023-10-20 19:37:13 +0700

    dart operator ternary

---
+++ bin/ternary_operator.dart
@@ -0,0 +1,12 @@
+void main() {
+  var nilai = 90;
+  var ucapan = nilai >= 75 ? 'Selamat anda lulus' : 'silahkan coba lagi';
+
+  // if (nilai >= 75) {
+  //   ucapan = 'Selamat Anda lulus';
+  // } else {
+  //   ucapan = 'silahkan coba lagi';
+  // }
+
+  print(ucapan);
+}
