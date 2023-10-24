fdbc7f1c75d08bbd4656e84fa8389c891e8a6da5
Muh Firdaus <muhammadfirdaus66538@gmail.com>
2023-10-24 21:08:00 +0700

    dart while and do while loop

---
+++ bin/do_while.dart
@@ -0,0 +1,7 @@
+void main() {
+  var counter = 1;
+  do {
+    print('perulangan ke-$counter');
+    counter++;
+  } while (counter <= 10);
+}
---
+++ bin/while.dart
@@ -0,0 +1,8 @@
+void main() {
+  var counter = 1;
+
+  while (counter <= 10) {
+    print('perulangan ke $counter');
+    counter++;
+  }
+}
